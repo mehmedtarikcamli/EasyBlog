bedc187969d2ce82d0ec3bef6403c18a7dbe0d75
Mehmed Tarık Çamlı <mehmedtarkc@gmail.com>
2024-02-07 10:21:14 +0300

    migration Ilk

---
+++ EasyBlog/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyBlog.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ EasyBlog/Data/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace EasyBlog.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
